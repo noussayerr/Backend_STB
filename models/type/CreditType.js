6c66e3b60907e0df52ef41053de2a5129946242b
noussayerr <noussayer.rahal.83@gmail.com>
2025-06-22 22:51:15 +0100

    fixing2

--- models/type/CreditType.js
+++ models/type/CreditType.js
@@ -22,4 +22,4 @@ const cardTypeSchema = new Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-export const CardType = mongoose.model("CreditType ", cardTypeSchema);
+export const CreditType = mongoose.model("CreditType ", cardTypeSchema);
